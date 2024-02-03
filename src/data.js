9ecb6bfe9751576306914d3ba64aa8f26f5431af
Amit Singh <amitkarun4559@gmail.com>
2024-02-03 15:46:07 +0530

    data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const ProjectsData = [
+  {
+    id: 1,
+    name: "Shopping Cart",
+    TechStacks: ["React", "Tailwind CSS", "Redux"],
+    image: "/Video/ShoppingCart.gif",
+    description: "This is a user-friendly shopping website leveraging React, Tailwind CSS, and Redux for a seamless and enjoyable shopping experience.",
+    link : "https://shopping-cart-five-virid.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Random Gif Generator",
+    TechStacks: ["React", "Tailwind CSS"],
+    image: "/Video/RandomGifs.gif",
+    description: "This is a Random Gifs Generator that allows users to generate both random gifs and select specific gifs, built with React and Tailwind CSS for an enjoyable gif discovery experience.",
+    link : "https://random-gifs-generator01.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Personal Portfolio Website",
+    TechStacks: ["React", "Tailwind CSS"],
+    image: "/Video/myPortfolioWebsite.gif",
+    description: "My personal portfolio website using React and Tailwind CSS, featuring a clean and minimal UI design.",
+    link : "https://github-profile-hunter.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "GitHub Profile Hunter",
+    TechStacks: ["HTML", "CSS", "JavaScript"],
+    image: "/Video/GithubHunter.gif",
+    description: "This is a GitHub Profile Hunter project where users can search for other GitHub profiles. It is crafted using HTML, CSS, and JavaScript.",
+    link : "https://github-profile-hunter.netlify.app/",
+  },
+];
+
+export default ProjectsData;
